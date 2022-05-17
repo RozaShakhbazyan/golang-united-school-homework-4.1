1b227b387466edebbda1c69ca51ce138b24e584e
r.shakhbazyan <roza.shakhbazyan-extern@invia.de>
2022-05-18 00:24:31 +0400

    Solved Reverse String problem

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,10 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	var runes []rune = []rune(input)
+	for i := len(runes) - 1; i >= 0; i-- {
+		output += string(runes[i])
+	}
+
 	return output
 }
